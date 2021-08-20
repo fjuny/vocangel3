96bcc57c9f6bbafbcabc5ebbf595c5f18f36e862
Fong Jun Yang <jyfong2010@gmail.com>
2021-08-20 11:51:47 +0800

    3

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: 'config.env' });
+require('dotenv').config();
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
