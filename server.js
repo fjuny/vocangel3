b46c9999d91957b37b1cb1b43c371e29291b3392
Fong Jun Yang <jyfong2010@gmail.com>
2021-08-20 11:49:10 +0800

    1

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ app.get('/update-user',services.update_user);
 
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 
