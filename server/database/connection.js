e67e7c58984ac5c69aac63f740dd2a8e09016bdc
Fong Jun Yang <jyfong2010@gmail.com>
2021-08-20 11:55:27 +0800

    5

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async() => {
     try{
         //connection string
-        const con = await mongoose.connect(process.env.MONGO_URI,{
+        const con = await mongoose.connect(process.env.MONGO_uri,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
